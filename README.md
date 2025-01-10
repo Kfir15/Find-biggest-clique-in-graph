bd0c9bf1b80f055510c3384f9aedb92ac3c6bbf8
Kfir15 <kfireitan115@gmail.com>
2025-01-10 21:46:19 +0200

    README commit

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# Find-biggest-clique-in-graph
-Find biggest clique in graph. We received code and had to optimize it.
+# Find biggest clique in graph
+
+<p align="center">
+  <a href="#description">Description</a> •
+  <a href="#project-overview">Project Overview</a> •
+  <a href="#to-run">To Run</a> •
+  <a href="#output-format">Output Format</a>
+</p>
+
+
+## Description  
+This code find the biggest clique in a graph.  
+This code done in course "Computer Structure" in Bar Ilan University. We received code and had to optimize it.
+
+## Project Overview  
+In this task, we are given graph G = (V,E), represented by an adjacency matrix A where -  
+* A[i,j] = 1 if there is an edge between vertices 𝑖  and 𝑗 
+* A[i,j] = 0 if there is no edge between these vertices  
+  
+
+The goal is to find the biggest subset such that for all vertices in the subset there is an edge between them.
+
+## To Run  
+1. Clone this repository.  
+
+2. Create a text file named `graph.txt` in the project folder.  
+   This file should contain the graph's adjacency matrix, which is essential for running the code.
+ 
+ 3. **To generate a random graph**, run the following Python script (replace `n` with the desired number of vertices in the graph):
+ ```bash
+ python create-matrix.py n > graph.txt
+ ```
+
+4. **To compile:**
+```bash
+# compile the code
+gcc -O0 main.c max-clique.c -o main
+```  
+
+5. **To run:**
+```bash
+# run the code
+./main < graph.txt
+```
+
+## Output Format  
+The output of the program will display the following information:
+```bash
+Clique Members: 0 2 4 5 7 8 12
+Size: 7
+Time spent: 0.0001590000
+```
---
+++ max-clique.c
@@ -0,0 +1,142 @@
+/* Kfir Eitan 328494836 */
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_VERTICES 100
+
+void printSolution(int vertices[], int size) {
+    // print the vertices
+    for (int *ptr = vertices; ptr < vertices + size; ptr++) {
+        printf("%d ", *ptr);
+    }
+    printf("\n");
+}
+
+bool isClique(int graph[MAX_VERTICES][MAX_VERTICES], int clique[], int size) {
+    
+    // pointer to the vertex
+    int *ptr1 = clique;
+    for (; ptr1 < clique + size; ptr1++) {
+        // get the vertex
+        const int v = *ptr1;
+
+        // loop unrolling on the rest of the vertices
+        int *ptr2 = ptr1 + 1;
+        for (; ptr2 < clique + size - 1; ptr2 += 2) {
+            // get two other vertices
+            const int u1 = *ptr2;
+            const int u2 = *(ptr2 + 1);
+
+            // check if the vertices are connected
+            if (!(graph[v][u1] | graph[u1][v]) | !(graph[v][u2] | graph[u2][v])) {
+                return false;
+            }
+        }
+        for (; ptr2 < clique + size - 1; ptr2++)
+        {
+            // check the last vertex
+            const int u = *ptr2;
+            if (!(graph[v][u] | graph[u][v])) {
+                return false;
+            }
+        }     
+    }
+    
+    return true;
+}
+
+
+void generateCombinations(int graph[MAX_VERTICES][MAX_VERTICES], int n, int *clique, int k, int start, int currentSize, int *maxSize, int *maxClique) {
+
+    // if k <= max size so we cant find bigger clique here
+    if (k <= *maxSize) {
+        return;
+    }
+
+    // if remaining vertices + current size < k, we cant find clique of size k
+    if (n - start + currentSize < k) {
+        return;
+    }
+
+    // if current size is equal to k
+    if (currentSize == k)
+    {
+        // if this is clique
+        if (isClique(graph, clique, k))
+        {
+            // change the max size 
+            *maxSize = k;
+            
+            // get pointers to the clique and maxClique
+            int *cliquePtr = clique;
+            int *maxCliquePtr = maxClique;
+            
+            // copy the clique to maxClique, loop unrolling
+            for (; cliquePtr < clique + k - 1; cliquePtr += 2, maxCliquePtr += 2) {
+                *maxCliquePtr = *cliquePtr;
+                *(maxCliquePtr + 1) = *(cliquePtr + 1);
+            }
+            for (; cliquePtr < clique + k; cliquePtr++, maxCliquePtr++) {
+                *maxCliquePtr = *cliquePtr;
+            }
+        }
+        return;
+    }
+    
+    // check all the vertices from start to n
+    for (int i = start; i < n; i++) {
+        
+        int *ptr = clique;
+        // check if the vertex can be added to the clique, it must be connected to all
+        bool canAdd = true;
+        for (; ptr < clique + currentSize; ptr++) {
+            // if it not connected to all so enter false and break
+            if (!(graph[i][*ptr] | graph[*ptr][i])) {
+                canAdd = false;
+                break;
+            }
+        }
+        if (!canAdd) {
+            // if the vertex cant be added to the graph so skip it and move to the next vertex
+            continue;
+        }
+        
+        /// add the vertex to the clique
+        clique[currentSize] = i;
+        generateCombinations(graph, n, clique, k, i + 1, currentSize + 1, maxSize, maxClique);
+    }
+}
+
+void findMaxClique(int graph[MAX_VERTICES][MAX_VERTICES], int n) {
+    // allocate the clique, maxClique and create maxSize
+    int *clique = (int *)malloc(n * sizeof(int));
+    int *maxClique = (int *)malloc(n * sizeof(int));
+    int maxSize = 0;
+
+    // loop unrolling to generate combination of the graph
+    int k = 1;
+    for (; k < n - 1; k += 2) {
+        generateCombinations(graph, n, clique, k, 0, 0, &maxSize, maxClique);
+        generateCombinations(graph, n, clique, k + 1, 0, 0, &maxSize, maxClique);
+
+        // if the maxSize of the clique is all the vertex exit
+        if (maxSize == n) {
+            break;
+        }        
+    }
+    for (; k <= n; k++) {
+        generateCombinations(graph, n, clique, k, 0, 0, &maxSize, maxClique);
+    }
+
+    // print the max clique
+    printf("Clique Members: ");
+    printSolution(maxClique, maxSize);
+    printf("Size: %d\n", maxSize);
+
+    // free the memory
+    free(clique);
+    free(maxClique);
+}
+
